88c4f3e864f05f488e8bc91d354b531d4fe62cad
rashmithakatakam <rashmithakatakam.13@gmail.com>
2026-07-28 15:25:30 +0530

    Tokens-Day1

---
+++ day1.py
@@ -0,0 +1,20 @@
+print("Hello Codegnan")
+name='codegnan'
+print(name)
+
+emailid="rashmithakatakam.13@gmail.com"
+print(emailid)
+
+email_id="rashmithakatakam.13@gmail.com"
+print(email_id)
+
+rashmitha=12
+print(rashmitha)
+
+batch="cse"
+print(batch)
+
+length=5
+breadth=3
+area=length*breadth
+print(area)
