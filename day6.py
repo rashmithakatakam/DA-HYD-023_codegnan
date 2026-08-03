1c6acb0c0fe7dbd38214a74f1c43a77f89fa89ac
rashmithakatakam <rashmithakatakam.13@gmail.com>
2026-08-03 16:41:13 +0530

    Loops

---
+++ day6.py
@@ -0,0 +1,120 @@
+'''
+control,statements --> Control of Flow of execution of a program
+                   --> Conditional Statements --> if,elif,else...
+                   -->Repetation statements(Loops) --> for,while (for with else)(while with else)
+                   --> Jumping Statements -->break,continue,pass
+'''
+#Loops -->Loops aafre helpful for repetition (Automative tasks)
+#for keyword will be helpful to iterate over a sequence/range
+#Syntax for(for keyword):
+'''
+for <temp_var> in sequence/range:
+    statements(s).....
+    .........
+
+#range(stop)-->default 0 ends at stop-1
+#range(start,stop,step) picks 0 as start value
+#by default range 
+for i in range(10):
+    print(i)
+#In above case we got 10 iterations    
+for i in range(1,10):
+    #if i > 5:
+        #print(f'value of i is-->{i}')
+    #now i want to get only even numbers with above condition
+    if i > 5 and i%2==0:
+        print(f'Final value of i is --> {i}')
+
+#range(start,stop,step)--> here step --> interval..
+for i in range(1,10,2):
+    print(i)
+    print("Done")
+for i in range(10,1,-1):
+    print(i)
+#print -10 to -1
+for i in range(-10,0,1):
+    print(i)
+
+#[] --> we generally lists
+names = ['rashmitha','akshaya','srujana']
+print(len(names))#len(obj)-->returns the number of items in a container
+for i in names:
+    print(i)
+for name in names:
+   #print(name)
+   #print(f'student Name is {name}')
+   if name == "rashmitha":
+       print(f"Student name is {name}")
+
+#calculate the sum of first 10 numbers
+#first understand your input --> range(11)-->10 numbers
+#second understand your output --> sum(number)
+#third we need to map the logic
+
+result=0 #target variable
+for i in range(11):
+    #print(i)
+    #print(f'result is {i+i}')
+    result = result +i #result += i
+    print(f'Now the result is {result}')
+print(f'sum of 10 numbers is {result}')
+
+
+#sum of first 10 even numbers
+result=0 #target variable
+for i in range(21):
+    if i%2==0:
+        print(i)
+        result = result +i #result += i
+        print(result)
+print(f'sum of 10 even numbers is {result}')
+'''
+
+#Understand the loops usage with fitness streak example
+#work_out--> 1,work_out_missed --> 0
+
+work_log = [0,1,1,1,0,1,0]
+#result variable --> longest_streak
+longest_streak = 0#target variable
+current_streak = 0
+for day in work_log:
+    if day == 1:
+        #print(day)
+        current_streak = current_streak + 1
+        if current_streak > longest_streak:
+            longest_streak = current_streak
+    else:
+        current_streak = 0 #streak breaks
+print(f'longest streak is {longest_streak}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
